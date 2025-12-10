e69bf66cdbe4c64edeedd7ed9cb38fe93dc8a0f7
Loic-Zhou <loiczhou166@gmail.com>
2025-12-10 21:18:10 +0100

    feat: chainage-avant

--- main.lisp
+++ main.lisp
@@ -32,8 +32,51 @@
   (initialiser-base-faits)
   ;; Chargement des règles depuis les données
   (charger-recettes)
+
+    ;   (pate_brisee
+    ;  ((farine 200) (oeuf 1) (eau 4) (beurre 100))
+    ;  (bol rouleau)
+    ;  nil nil vegetarien)
+
+    ; (pate_crepe
+    ;  ((beurre 40) (oeuf 4) (farine 250) (lait 60) (sucre 40))
+    ;  (bol fouet)
+    ;  nil nil vegetarien)
+    ; (quiche_lorraine
+    ;  ((beurre 20) (oeuf 8) (creme 40) (lard 180) (gruyere 70) (pate_brisee 1))
+    ;  (moule_tarte fouet bol)
+    ;  (printemps ete automne hiver)
+    ;  (plat entree)
+    ;  nil)
+    ;     (tarte_tatin
+    ;  ((pomme 15) (sucre 100) (eau 5) (beurre 50) (pate_brisee 1))
+    ;  (moule_tarte casserole)
+    ;  (automne hiver)
+    ;  dessert
+    ;  vegetarien)
+    (format t "~%~%~%~%~%~%~%~%~%~%")
+    (ajouter-fait 'ingredients 'pomme 150)
+    (ajouter-fait 'ingredients 'creme 100)
+    (ajouter-fait 'ingredients 'lard 200)
+    (ajouter-fait 'ingredients 'gruyere 100)
+    (ajouter-fait 'ingredients 'farine 500)
+    (ajouter-fait 'ingredients 'oeuf 60)
+    (ajouter-fait 'ingredients 'eau 1000)
+    (ajouter-fait 'ingredients 'beurre 500)
+    (ajouter-fait 'ingredients 'lait 200)
+    (ajouter-fait 'ingredients 'sucre 300)
+    (ajouter-fait 'materiel 'bol t)
+    (ajouter-fait 'materiel 'rouleau t)
+    (ajouter-fait 'materiel 'fouet t)
+    (ajouter-fait 'materiel 'moule_tarte t)
+    (ajouter-fait 'materiel 'casserole t)
+    (afficher-base-faits)
+    (chainage-avant)
+    (print *solutions-trouvees*)
+    (format t "~%")
   ;; Lancement de l'interface utilisateur
-  (interface-principale))
+  ;(interface-principale)
+  )
 
 
 
